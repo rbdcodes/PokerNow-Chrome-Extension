2253095978ea423e5dac934ef1d53788aee5b86f
Rodney Deransburg <rodney.deransburg@gmail.com>
2023-08-23 16:25:20 -0500

    Starting Refactor to only use chrome extension :(

--- UI/background.js
+++ UI/background.js
@@ -6,6 +6,18 @@
 //   }
 // });
 
+chrome.webRequest.onBeforeRequest.addListener(
+  (details) => {
+    console.log("webRequest V");
+    console.log(details);
+    if (details.method === "POST") {
+      console.log("hello");
+    }
+  },
+  { urls: ["<all_urls>"], types: ["xmlhttprequest"] },
+  ["requestBody"]
+);
+
 chrome.tabs.onActivated.addListener(function (tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var activeTab = tabs[0];
--- UI/content.js
+++ UI/content.js
@@ -1,50 +1,48 @@
-// const elem = document.createElement("div");
-// // elem.appendChild(document.createTextNode("test"));
-// elem.classList.add("actionContainer");
+const elem = document.createElement("div");
+// elem.appendChild(document.createTextNode("test"));
+elem.classList.add("actionContainer");
 
-// const addButton = document.createElement("button");
-// addButton.textContent = "add";
-// addButton.id = "addButton";
+const addButton = document.createElement("button");
+addButton.textContent = "add";
+addButton.id = "addButton";
 
-// const action2 = document.createElement("div");
-// action2.textContent = "lul";
-// action2.classList.add("component27");
+const action2 = document.createElement("div");
+action2.textContent = "lul";
+action2.classList.add("component27");
 
-// const action1 = document.createElement("div");
-// action1.textContent = "lul";
-// action1.classList.add("component27");
+const action1 = document.createElement("div");
+action1.textContent = "lul";
+action1.classList.add("component27");
 
-// addButton.addEventListener("click", () => {
-//   const newComponent = document.createElement("div");
-//   newComponent.className = "component27";
-//   newComponent.textContent = "lul";
-//   elem.appendChild(newComponent);
-// });
+addButton.addEventListener("click", () => {
+  const newComponent = document.createElement("div");
+  newComponent.className = "component27";
+  newComponent.textContent = "lul";
+  elem.appendChild(newComponent);
+});
 
-// elem.appendChild(addButton);
-// elem.appendChild(action1);
-// elem.appendChild(action2);
+elem.appendChild(addButton);
+elem.appendChild(action1);
+elem.appendChild(action2);
 
-// window.onload = function () {
-//   // Your code here
-//   console.log("yum");
-//   let containerDiv = document.querySelector(".table-player.table-player-1");
+window.onload = function () {
+  // Your code here
+  console.log("yum");
+  let containerDiv = document.querySelector(".table-player.table-player-1");
 
-//   containerDiv.appendChild(elem);
-// };
-// chrome.runtime.onConnect.addListener((port) => {
-//   console.log("connected ", port);
-
-//   if (port.name === "hi") {
-//     port.onMessage.addListener(this.processMessage);
-//   }
-// });
+  containerDiv.appendChild(elem);
+};
 
 chrome.runtime.sendMessage({ message: "testtt from content" }, (response) => {
-  console.log(response.message);
+  console.log(`from runTime sendMessage: ${response.message}`);
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
+  console.log(`From onMessage!: ${message}`);
+  const newComponent = document.createElement("div");
+  newComponent.className = "component27";
+  newComponent.textContent = "lul";
+  elem.appendChild(newComponent);
+
   sendResponse({ message: "response from content js" });
 });
